9f18c60e3f65c3afcf3bc49928f4c7d9b8084d0d
sneha <64890439+snehadugani2127@users.noreply.github.com>
2024-02-21 13:29:36 +0530

    Update streamlit_app.py
    added text for fav fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ streamlit.title('Hello streamlit, you just got a new buddy')
 streamlit.header('My Name is Sneha 👸🏻')
 streamlit.text('🖥️ I am a software engineer')
 streamlit.text('🤓 i love data and I love to work with data...')
+
+streamlit.header('🥭 🍍My fav Fruits 🥑 🍓')
+streamlit.text('🥭 mango is the most fav fruit of mine')
+streamlit.text('🍓 strawberry is the seasonal fruit which I like')
+streamlit.text('🍍 pinaaple is one of my fav tangy fruit')
