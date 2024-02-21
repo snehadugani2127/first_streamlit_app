11a90c3c6aef5355e1536cf9809491171a4043a9
sneha <64890439+snehadugani2127@users.noreply.github.com>
2024-02-21 13:23:27 +0530

    Update streamlit_app.py
    added emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('Hello streamlit, you just got a new buddy')
-streamlit.header('My Name is Sneha')
-streamlit.text('I am a software engineer')
-streamlit.text('i love data and I love to work with data...')
+streamlit.header('My Name is Sneha 👸🏻')
+streamlit.text('🖥️ I am a software engineer')
+streamlit.text('🤓 i love data and I love to work with data...')
