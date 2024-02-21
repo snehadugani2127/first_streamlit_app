2998a8b1bd655b7ae0327d8d925f0bf0429adcf1
sneha <64890439+snehadugani2127@users.noreply.github.com>
2024-02-21 13:17:52 +0530

    Update streamlit_app.py
    added hader and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('Hello streamlit, you just got a new buddy')
+streamlit.Header('My Name is Sneha')
+streamlit.text('I am a software engineer')
+streamlit.text('i love data and I love to work with data...')
